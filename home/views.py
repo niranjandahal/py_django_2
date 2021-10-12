6601b487dc4ba34301a1f33809ee484365d25d5b
niranjandahal <niranjandahal76@gmail.com>
2021-10-12 15:50:20 +0545

    secondprojectincomplete

---
+++ home/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+
+def login(request):
+    return HttpResponse("this is an Register page")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def index(request):
+#     return HttpResponse("home, world. You're at the polls index.")
